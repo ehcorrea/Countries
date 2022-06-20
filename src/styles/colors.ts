4c6b258a291d5292c9ccbdea274533dc72fa215d
Emilio Correa <ehcorrea60@gmail.com>
2022-06-20 20:10:30 -0300

    refactor: 💡 Theme
    changed theme colors logic

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -4,11 +4,13 @@ export default {
     background: '#fafafa',
     input: '#858585',
     text: '#111517',
+    black: '#000000',
   },
   dark: {
     elements: '#2b3945',
     background: '#202c37',
     input: '#858585',
     text: '#ffffff',
+    black: '#000000',
   },
 } as const;
--- src/types/style-components.d.ts
+++ src/types/style-components.d.ts
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import theme from '../styles/theme';
+import colors from '../styles/colors';
 
 type Theme = typeof theme;
+type ColorsDefault = typeof colors['default'];
+type ColorsDark = typeof colors['dark'];
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {
-    colors: {
-      elements: string;
-      background: string;
-      input: string;
-      text: string;
-    };
+    colors: ColorsDefault | ColorsDark;
   }
 }
