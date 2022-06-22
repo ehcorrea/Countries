45fbe94185c9d6a4f847c583853d1953481bbe55
Emilio Correa <ehcorrea60@gmail.com>
2022-06-22 00:37:36 -0300

    feat: 🚀 view <Home/>
    added components dropdown and country list

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList, ActivityIndicator } from 'react-native';
 
-import { Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { SearchInput, Dropdown, CountryCard } from '../../components';
+
+import { useGetCountriesQuery } from '../../service/countries';
 
 import * as S from './styles';
+import { ProfileScreenNavigation } from '../../routes/types';
 
 const Home = () => {
+  const { navigate } = useNavigation<ProfileScreenNavigation>();
+
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const [filterValue, setFilterValue] = useState('');
+
+  const { data, isLoading } = useGetCountriesQuery(searchInputValue);
+
+  const handleChangeSearchInputValue = (text: string) => {
+    setSearchInputValue(text);
+  };
+
+  const handleChangeFilterValue = (text: string) => {
+    setFilterValue(text);
+  };
+
+  const handleCountryDetail = (alphaCode: string) => {
+    navigate('Detail', { code: alphaCode });
+  };
+
   return (
     <S.Wrapper>
-      <Text>Home</Text>
+      <SearchInput
+        onChangeText={handleChangeSearchInputValue}
+        debounce={1000}
+      />
+
+      <S.DropdownWrapper>
+        <Dropdown
+          onChangeValue={handleChangeFilterValue}
+          placeHolder="Filter by Region"
+          options={[
+            { label: 'Africa', value: 'Africa' },
+            { label: 'Americas', value: 'Americas' },
+            { label: 'Asia', value: 'Asia' },
+            { label: 'Europe', value: 'Europe' },
+            { label: 'Oceania', value: 'Oceania' },
+          ]}
+        />
+      </S.DropdownWrapper>
+
+      <S.ListWrapper>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="gray" />
+        ) : (
+          <FlatList
+            data={
+              filterValue
+                ? data?.filter((item) => item.region === filterValue)
+                : data
+            }
+            renderItem={({ item }) => (
+              <CountryCard
+                {...item}
+                onPress={() => handleCountryDetail(item.alpha3Code)}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item.alpha3Code}
+            ItemSeparatorComponent={S.ListSeparator}
+          />
+        )}
+      </S.ListWrapper>
     </S.Wrapper>
   );
 };
--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -3,5 +3,32 @@ import styled, { css } from 'styled-components/native';
 export const Wrapper = styled.SafeAreaView`
   ${({ theme }) => css`
     background-color: ${theme.colors.background};
+    flex: 1;
+    padding-horizontal: ${theme.spacings.medium};
+    padding-top: ${theme.spacings.large};
+    align-items: center;
+  `}
+`;
+
+export const DropdownWrapper = styled.View`
+  ${({ theme }) => css`
+    padding-top: ${theme.spacings.xxlarge};
+    padding-bottom: ${theme.spacings.large};
+    width: 100%;
+  `}
+`;
+
+export const ListWrapper = styled.View`
+  ${({ theme }) => css`
+    flex: 1;
+    padding-horizontal: ${theme.spacings.xxlarge};
+    width: 100%;
+    justify-content: center;
+  `}
+`;
+
+export const ListSeparator = styled.View`
+  ${({ theme }) => css`
+    height: ${theme.spacings.xlarge};
   `}
 `;
