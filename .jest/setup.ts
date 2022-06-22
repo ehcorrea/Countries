6b1f5894f2aa4ec3d285670132c83dd1e085ea8d
Emilio Correa <ehcorrea60@gmail.com>
2022-06-21 21:27:59 -0300

    fix: 🐛 Animated
    added animated mock to setup

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -2,3 +2,5 @@ import '@testing-library/jest-native';
 import '@testing-library/jest-native/extend-expect';
 import 'jest-styled-components';
 import 'whatwg-fetch';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
